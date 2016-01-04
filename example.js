36218ce2f699992a8ad9f95dfdf28129922aac63
Olivier Monnier <omonnier.dev@gmail.com>
2016-01-04 14:10:09 +0100

    add basic generator

--- example.js
+++ example.js
@@ -2,10 +2,13 @@ var util = require('util');
 
 var tokenizer = require('./tokenizer');
 var parser = require('./parser');
+var generator = require('./generator');
 
 var text = '(def avg (x y) ( / (+ x y ) 2)) (def addOne (x) (+ x 1)) (test = 0) (if (test === 1) (do (+ test 1)) (else (- test 1)))';
 
 var tokens = tokenizer(text);
 var tree = parser(tokens);
+var output = generator(tree.roots);
 
 console.log(util.inspect(tree, false, null));
+//console.log(output);
